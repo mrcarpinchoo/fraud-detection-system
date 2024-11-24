1d16904e8f3a66937cd57ed633e4f4e2e9cc0855
mrcarpinchoo <guillermo.romero@iteso.mx>
2024-11-23 22:35:25 -0600

    Modify mongodb/db/config.py to load environment variables from .env file. Modify requirements.txt to include python-dotenv module

--- mongodb/db/config.py
+++ mongodb/db/config.py
@@ -1,6 +1,11 @@
 # core modules
 import os
 
+# third-party modules
+from dotenv import load_dotenv
+
+load_dotenv() # loads environment variables from .env file
+
 # environment variables
-MONGODB_CONNECTION_URI = os.getenv("MONGODB_CONNECTION_URI", "mongodb://localhost:27017")
-MONGODB_DATABASE_NAME = os.getenv("MONGODB_DATABASE_NAME", "iteso")
+MONGODB_URI = os.getenv("MONGODB_URI", "mongodb://localhost:27017")
+MONGODB_NAME = os.getenv("MONGODB_NAME")
--- mongodb/db/connection.py
+++ mongodb/db/connection.py
@@ -4,6 +4,6 @@ from pymongo import MongoClient
 # custom modules
 import mongodb.db.config as config 
 
-mongoClient = MongoClient(config.MONGODB_CONNECTION_URI) # creates client for a MongoDB instance
+mongoClient = MongoClient(config.MONGODB_URI) # creates client for a MongoDB instance
 
-database = mongoClient[config.MONGODB_DATABASE_NAME] # positions the client on the specified database
+database = mongoClient[config.MONGODB_NAME] # positions the client on the specified database
